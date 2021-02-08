babc6879b9ae83eb0e49c2b7b5806bb001c15e61
Polina Artamonova <p.artamonova@gmail.com>
2021-02-08 09:39:26 +0100

    dynamic data on product

---
+++ product.js
@@ -0,0 +1,17 @@
+const url = "https://kea-alt-del.dk/t7/api/products/1526";
+//fetch the data
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
+
+//populate the page
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".information .brand").textContent = product.brandname;
+  document.querySelector(".information .productname").textContent =
+    product.productdisplayname;
+  document.querySelector(
+    "img.productimage"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+  document.querySelector("img.productimage").alt = product.productdisplayname;
+}
